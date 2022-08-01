9d000112f0fbe61c5fa17a23c67ca7ec13c7a04c
Alferov Viktor <alferovvictor@yandex.ru>
2022-08-01 13:32:42 +0200

    * minor changes

---
+++ AsyncEnum/AsyncEnum/Controllers/ValuesApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncEnum.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ValuesApiController : ControllerBase
+	{
+		[HttpPost]
+		[ProducesResponseType(type: typeof(Model2), statusCode: StatusCodes.Status200OK)]
+		public Model2 GetModel([FromBody] Model2 model)
+		{
+			return model;
+		}
+	}
+
+	public class Model1
+	{
+		public string value { get; set; }
+	}
+
+	public class Model2: Model1
+	{
+		public string value2 { get; set; }
+	}
+}
--- AsyncEnum/AsyncEnum/Program.cs
+++ AsyncEnum/AsyncEnum/Program.cs
@@ -3,6 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSwaggerGen(swg =>
+{
+    /*swg.UseAllOfForInheritance();
+    swg.SelectSubTypesUsing(baseType =>
+    {
+        return baseType.Assembly.GetTypes().Where(type => type.IsSubclassOf(baseType));
+    });*/
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -10,6 +19,9 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -24,4 +36,3 @@ app.UseEndpoints(conf =>
 });
 
 app.Run();
-;
